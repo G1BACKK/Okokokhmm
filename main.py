2e63e798971af79120b5b453dd588026dcb252ae
G1BACKK <g1g4blast@gmail.com>
2025-11-20 00:00:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+from time import sleep
+from pyrogram import Client
+
+API_ID = int(os.environ["API_ID"])
+API_HASH = os.environ["API_HASH"]
+PHONE = os.environ["PHONE"]
+CHANNEL = os.environ["CHANNEL"]
+
+app = Client("session", api_id=API_ID, api_hash=API_HASH, phone_number=PHONE)
+
+def check_live():
+    try:
+        chat = app.get_chat(CHANNEL)
+        if chat.video_chat:
+            print("Live stream detected! Joining...")
+            app.join_group_call(chat.id)
+        else:
+            print("No live stream now…")
+    except Exception as e:
+        print("Error:", e)
+
+with app:
+    while True:
+        check_live()
+        sleep(10)
